7b924a1854a6576d86a0e29f854cbae6847d6e28
muhammod1 <muhammodholawale@gmail.com>
2022-09-26 21:13:07 +0100

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { Box, Text } from '@chakra-ui/react'
 
 export const Navbar = () => {
   return (
-      <Box
-        pos="fixed"
-        padding="20px 70px"
-        bg="bgDark"
-        color="white"
-        w="100%"
-      >
-        <Text color="white" fontSize="28px" fontWeight="600">Muhaj Dev</Text>
-      </Box>
+    <Box
+      pos="fixed"
+      padding="20px 70px"
+      bg="bgDark"
+      color="white"
+      w="100%"
+    >
+      <Text color="white" fontSize="28px" fontWeight="600">Muhaj Dev</Text>
+    </Box>
   )
 }
