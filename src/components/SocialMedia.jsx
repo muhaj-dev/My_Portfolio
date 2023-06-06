6277684cf696811ac9fdaa1c251348ec125f77d1
muhammod1 <muhammodholawale@gmail.com>
2023-06-06 21:07:27 +0100

    Minor edit

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href='https://twitter.com/muhaj_dev?t=pU4ZlMNoswLKsoJaYRAHaw&s=09'>
+        <BsTwitter />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+    <a href='https://github.com/muhammod1'>
+      <FaGithub />
+    </a>
+
     </div>
     <div>
-      <BsInstagram />
+    <a href='https://www.linkedin.com/in/muhammod-ajibade-6111911b5'>
+      <FaLinkedinIn />
+    </a>
+
     </div>
   </div>
 );
