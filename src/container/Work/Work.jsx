0a6b869f4902820d135e82c407012b2fcc3cdd0a
muhammod1 <muhammodholawale@gmail.com>
2023-06-06 19:52:16 +0100

    Minor edit

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -42,7 +42,7 @@ const Work = () => {
     <>
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
-      <div className="app__work-filter">
+      {/* <div className="app__work-filter">
         {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
@@ -52,7 +52,7 @@ const Work = () => {
             {item}
           </div>
         ))}
-      </div>
+      </div> */}
 
       
       <motion.div
