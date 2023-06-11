4b88e01e9cad852d757438d4a8e15224993a8a27
muhammod1 <muhammodholawale@gmail.com>
2023-06-11 22:43:20 +0100

    minor commit

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['HTML & CSS','Vite', 'NextJS', 'React JS', 'All'].map((item, index) => (
+        {['HTML & CSS','Vite', 'NextJS', 'React', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
